1ac2e3434300fcb8e24d55e964d8fa43a3a7f721
BrilStrawhat <yerofiev@gmail.com>
2020-07-06 21:53:36 +0300

    conflict

--- server/src/server.c
+++ server/src/server.c
@@ -97,6 +97,29 @@ int main(int argc, char **argv) {
 
     *online_users = g_hash_table_new_full(g_int_hash, NULL,
                                           NULL, NULL);
+    GError * error = NULL;
+
+    // database variable
+    char *errmsg = NULL;
+    sqlite3 *db;
+    int rc = 0;
+
+    // database setup start
+    if ((rc = sqlite3_open("uchat_db", &db)) != SQLITE_OK) {
+            g_error("Can't open database: %s\n", sqlite3_errmsg(db));
+            sqlite3_close(db);
+    }
+    if ((rc = sqlite3_exec(db, "SELECT User_id FROM users_credential LIMIT 1;",
+                    NULL, NULL, &errmsg)) != SQLITE_OK) {
+        if ((rc = sqlite3_exec(db, "CREATE TABLE users_credential ( \
+                            user_id INT NOT NULL, \
+                            login VARCHAR(50) NOT NULL, \
+                            passwd_hash VARCHAR(32) NOT NULL, \
+                            PRIMARY KEY(user_id, login));", NULL, NULL, &errmsg)) != SQLITE_OK) {
+                g_error("Failed to create table: %s\n", errmsg);
+                sqlite3_close(db);
+        }
+    }
     service = g_socket_service_new();
     g_socket_listener_add_inet_port((GSocketListener*)service, 5050, NULL, &error);
     g_signal_connect(service, "incoming", G_CALLBACK(incoming_callback), NULL);
