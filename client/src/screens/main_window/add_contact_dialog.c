e5e6165adbc8a82c4a1dd492fd313e1724a80b3a
BrilStrawhat <yerofiev@gmail.com>
2020-07-25 17:04:15 +0300

    get_contac finished

--- client/src/screens/main_window/add_contact_dialog.c
+++ client/src/screens/main_window/add_contact_dialog.c
@@ -29,17 +29,17 @@ static void find_user(GtkEntry *entry, t_client *client) {
     const gchar *input = gtk_entry_get_text(entry);
     gchar *request = NULL;
 
-    gtk_widget_hide(GTK_WIDGET(info));
-    if (strlen(input) != 13) {
-        gtk_label_set_text(info, "Enter full number");
-        gtk_widget_show(GTK_WIDGET(info));
-        return;
-    }
-    if (mx_match(input, MX_ALLOW_PATTERN, 0, 0)) {
-        gtk_label_set_text(info, "Not valid phone number");
-        gtk_widget_show(GTK_WIDGET(info));
-        return;
-    }
+    // gtk_widget_hide(GTK_WIDGET(info));
+    // if (strlen(input) != 13) {
+        // gtk_label_set_text(info, "Enter full number");
+        // gtk_widget_show(GTK_WIDGET(info));
+        // return;
+    // }
+    // if (mx_match(input, MX_ALLOW_PATTERN, 0, 0)) {
+        // gtk_label_set_text(info, "Not valid phone number");
+        // gtk_widget_show(GTK_WIDGET(info));
+        // return;
+    // }
     request = mx_find_contact_request((gchar *)input, "client->token");
     mx_send_data(client->data_out, request);
     g_free(request);
