3f298989960ecd97a9ee011c0cba759059411ef4
Hvvang <shemedvedd@gmail.com>
2020-07-27 19:05:13 +0300

    minor bug fix and changing ui

--- client/src/screens/main_window/add_contact_dialog.c
+++ client/src/screens/main_window/add_contact_dialog.c
@@ -1,7 +1,7 @@
 #include "client.h"
 
 #define MX_STYLES "./src/screens/main_window/dialog.css"
-#define MX_MAIN_WINDOW "./src/screens/glade/main.glade"
+#define MX_MAIN_WINDOW "./src/screens/glade/new_contact.glade"
 #define MX_ALLOW_PATTERN "/^[+]{1}[0-9]{12}$"
 
 static gboolean clear_inputs(GtkWidget *widget, GdkEventKey *event,
@@ -63,7 +63,8 @@ GtkWindow *mx_add_contact_dialog(t_client *client) {
     client->contact_view = NULL;
     gtk_entry_set_text(search, "");
     g_signal_connect(search, "activate", G_CALLBACK(find_user), client);
-    gtk_window_set_decorated(window, FALSE);
+    // gtk_window_set_decorated(window, FALSE);
+    gtk_window_set_transient_for(window, client->main_window);
     g_signal_connect(window, "key_press_event",
                      G_CALLBACK(clear_inputs), search);
     return window;
